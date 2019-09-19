40c943c2db95550e90c6fa8f4e2cfff893072b5c
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2019-09-19 18:45:56 -0300

    setup

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -3,10 +3,10 @@ class IncomingMailsController < ApplicationController
     protect_from_forgery with: :null_session
 
     def create
-      puts "asldhakjsdkjahsdjahsd"
-      Rails.logger.debug params.inspect
+      puts "Receiving email...."
       Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
-      redirect_to received_path(params)
+      
+      redirect_to received_path(params.require(:headers))
     end
 
 end
