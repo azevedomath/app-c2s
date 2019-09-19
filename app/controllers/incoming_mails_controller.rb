6d14d2c0447872ff7d9263c9ac8e2a3e0d657057
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2019-09-19 18:26:23 -0300

    adding cloudmailin

---
+++ app/controllers/incoming_mails_controller.rb
@@ -0,0 +1,11 @@
+class IncomingMailsController < ApplicationController
+
+    protect_from_forgery with: :null_session
+
+    def create
+      puts "asldhakjsdkjahsdjahsd"
+      Rails.logger.debug params.inspect
+      Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
+    end
+
+end
